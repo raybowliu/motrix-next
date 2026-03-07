b255924b3331c21c7120781304cf8cacf77b6035
sekiro <qq1326555262@gmail.com>
2026-03-07 19:43:50 +1100

    feat: Add i18n support by integrating `@intlify/unplugin-vue-i18n` into Vite configuration and dependencies.

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,20 @@
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import UnoCSS from 'unocss/vite'
+import VueI18nPlugin from '@intlify/unplugin-vue-i18n/vite'
 import { resolve } from 'path'
 
 const host = process.env.TAURI_DEV_HOST
 
 export default defineConfig(async () => ({
-  plugins: [vue(), UnoCSS()],
+  plugins: [
+    vue(),
+    UnoCSS(),
+    VueI18nPlugin({
+      include: resolve(__dirname, 'src/shared/locales/**'),
+      runtimeOnly: true,
+    }),
+  ],
   resolve: {
     alias: {
       '@': resolve(__dirname, 'src'),
